dba4fd01b5b3b458e140ff71080da4c4c21e26e4
{Ninanote} <{ninanyama@yahoo.co.jp}>
2024-06-14 00:37:34 +0900

    x,y平面でのグリッドセルを作る

--- source/support.cpp
+++ source/support.cpp
@@ -13,48 +13,44 @@
 using namespace std;
 using namespace Eigen;
 
-//六角形のクラス
-struct hexagon
-{
-    int index; //六角形の番号
-    vector<int> hi; //今見ている六角形の回りの六角形のindex
-};
+// x,y平面でのグリッドセルを作る
+vector<Vector2d> grid_cell(vector<Vector3d> V){
+    vector<Vector2d> gc;
+    
+    double x_min=100,x_max=-100,y_min=100,y_max=-100;
+    for(int i=0;i<V.size();i++){
+        if(x_min>V[i](0)){
+            x_min=V[i](0);
+        }
+        if(y_min>V[i](1)){
+           y_min=V[i](1); 
+        }
+        if(x_max<V[i](0)){
+            x_max=V[i](0);
+        }
+        if(y_max<V[i](1)){
+            y_max=V[i](1);
+        }
+    }
 
-// 60°の計算
-Vector3d make_etriangle(Vector3d p,Vector3d pd,Vector3d n,double length){
-    Vector3d d;
-    MatrixXd A(3,3);
-    A.row(0)=p-pd;
-    A.row(1)=pd-p;
-    A.row(2)=n;
-    Vector3d B(3);
-    B(0)=(1/2)*length*length+(pd.dot(p))-pd.dot(pd);
-    B(1)=(1/2)*length*length+(pd.dot(p))-p.dot(p);
-    B(2)=0;
-    d=A.colPivHouseholderQr().solve(B);
-    return d;
+    gc[0](0)=x_min;
+    gc[0](1)=y_min;
+    
+    Vector2d g;
+    for(int i=y_min;i<=y_max;i+=y_max/100){
+        for(int j=x_min;j<=x_max;j+=x_max/100){
+            g(0)=i;
+            g(1)=j;
+            gc.push_back(g);
+        }
+    }
+
+    return gc;
 }
 
-// 点の内外判定
+
 bool point_in_out(Vector3d oh,Vector2d s){
 
-}
-
-// ハニカムサンプリング(入力：最小包含の座標大小,一辺の長さ)
-vector<Vector2d> honeycomb(Vector2d min,Vector2d max,double r){
-    vector<Vector2d> sample;
-    double i=min(0),j=min(1);
-    while(j<max(1)){ //最大のy座標
-        i=min(0);
-        while(i<max(0)){ //最大のx座標
-            Vector2d center{i,j};
-            vector<Vector2d> mh=make_hexagon(center,r);
-            sample.insert(sample.end(),mh.begin(),mh.end());
-            i+=r*3;
-        }
-        j+=r*sin(1.04719755);
-    }
-    return sample;
 }
 
 // サポート構築(Lattice)
@@ -91,7 +87,8 @@ int main(int argc,char* argv[])
     // }
 
     // 面のオーバハングを調べる
-    double angle=0.5235987755982988; //閾値:cura35度(0.6108652381980153)，45度：0.7853981633974483
+    cout<<"面のオーバーハングを調べる"<<F.size()<<endl;
+    double angle=0.6108652381980153; //閾値:cura35度(0.6108652381980153)，45度：0.7853981633974483
     Vector3d direction(0,-1,0); //造形方向ベクトル
     vector<int> num; //オーバーハングかどうか(1 or 0)
     for(int i=0;i<F.size();i++){
