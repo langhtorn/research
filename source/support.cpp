d443dd51451f8acd65e1e231ef58b7ecd29b9aba
langhtorn <langhtorn>
2024-06-14 16:50:27 +0900

    x,z平面に平行な面のy座標未修正

--- source/support.cpp
+++ source/support.cpp
@@ -13,39 +13,59 @@
 using namespace std;
 using namespace Eigen;
 
-// x,y平面でのグリッドセルを作る
+// 底面(x,z平面)でのグリッドセルを作る(入力：モデルの座標)
 vector<Vector2d> grid_cell(vector<Vector3d> V){
     vector<Vector2d> gc;
     
-    double x_min=100,x_max=-100,y_min=100,y_max=-100;
+    double x_min=100,x_max=-100,z_min=100,z_max=-100;
     for(int i=0;i<V.size();i++){
         if(x_min>V[i](0)){
             x_min=V[i](0);
         }
-        if(y_min>V[i](1)){
-           y_min=V[i](1); 
+        if(z_min>V[i](2)){
+           z_min=V[i](1); 
         }
         if(x_max<V[i](0)){
             x_max=V[i](0);
         }
-        if(y_max<V[i](1)){
-            y_max=V[i](1);
+        if(z_max<V[i](2)){
+            z_max=V[i](2);
         }
     }
 
-    gc[0](0)=x_min;
-    gc[0](1)=y_min;
-    
     Vector2d g;
-    for(int i=y_min;i<=y_max;i+=y_max/100){
-        for(int j=x_min;j<=x_max;j+=x_max/100){
+    g(0)=x_min;
+    g(1)=z_min;
+    gc.push_back(g);
+    // cout<<"x_min="<<x_min<<" y_min="<<y_min<<endl;
+
+    double x_length=x_max-x_min; //横の長さ
+    double y_length=z_max-z_min; //縦の長さ
+    for(double i=z_min+y_length/100;i<=z_max;i+=y_length/100){
+        for(double j=x_min+x_length/100;j<=x_max;j+=x_length/100){
+            // cout<<"("<<i<<" ,"<<j<<")"<<endl;
             g(0)=i;
             g(1)=j;
             gc.push_back(g);
         }
     }
 
+    // for(int i=0;i<V.size();i++){
+    //     cout<<gc[i]<<endl;
+    // }
+
     return gc;
+}
+
+// 点だけobjファイル出力(二次元)
+void obj_out(vector<Vector2d> vert,const char* file_name){
+    FILE* f;
+    f=fopen(file_name,"w");
+    
+    for(int i=0;i<vert.size();i++){
+        cout<<vert[i](0)<<","<<vert[i](1)<<endl;
+        fprintf(f,"v %lf 0 %lf\n",vert[i](0),vert[i](1));
+    }
 }
 
 
@@ -79,6 +99,7 @@ int main(int argc,char* argv[])
         fp=fopen(argv[1],"r");
     }
     reado.readPoint(V,F,fn);
+    cout<<"読み込み完了\n";
     // for(int i=0;i<V.size();i++){
     //     cout<<V[i]<<endl;
     // }
@@ -86,38 +107,43 @@ int main(int argc,char* argv[])
     //     cout<<F[i]<<endl;
     // }
 
-    // 面のオーバハングを調べる
-    cout<<"面のオーバーハングを調べる"<<F.size()<<endl;
-    double angle=0.6108652381980153; //閾値:cura35度(0.6108652381980153)，45度：0.7853981633974483
-    Vector3d direction(0,-1,0); //造形方向ベクトル
-    vector<int> num; //オーバーハングかどうか(1 or 0)
-    for(int i=0;i<F.size();i++){
-        if(overh(angle,direction,V[F[i](0)],V[F[i](1)],V[F[i](2)])) num.push_back(1);
-        else num.push_back(0);
-    }
+    // x,y平面グリッドセルの作成
+    vector<Vector2d> gc=grid_cell(V);
+    cout<<"グリッドセルの作成完了\n";
+    obj_out(gc,"grid_cell.obj");
 
-    // vtk出力，結果の可視化
-    FILE* fpv;
-    fpv=fopen("overhang.vtk","w");
-    fprintf(fpv,"# vtk DataFile Version 2.0\n");
-    fprintf(fpv,"Title of my super cool VTK file\nASCII\nDATASET UNSTRUCTURED_GRID\n");
-    fprintf(fpv,"POINTS %d float\n",(int)V.size());
-    for(int i=0;i<V.size();i++){
-        fprintf(fpv,"%lf %lf %lf\n",V[i](0),V[i](1),V[i](2));
-    }
-    fprintf(fpv,"CELLS %d %d\n",(int)F.size(),(int)F.size()*4);
-    for(int i=0;i<F.size();i++){
-        fprintf(fpv,"3 %d %d %d\n",F[i](0),F[i](1),F[i](2));
-    }
-    fprintf(fpv,"CELL_TYPES %d\n",(int)F.size());
-    for(int i=0;i<F.size();i++){
-        fprintf(fpv,"5\n");
-    }
-    fprintf(fpv,"CELL_DATA %d\n",(int)F.size());
-    fprintf(fpv,"SCALARS my_face_attr int 1\nLOOKUP_TABLE default\n");
-    for(int i=0;i<num.size();i++){
-        fprintf(fpv,"%d\n",num[i]);
-    }
+    // // 面のオーバハングを調べる
+    // cout<<"面のオーバーハングを調べる"<<F.size()<<endl;
+    // double angle=0.6108652381980153; //閾値:cura35度(0.6108652381980153)，45度：0.7853981633974483
+    // Vector3d direction(0,-1,0); //造形方向ベクトル
+    // vector<int> num; //オーバーハングかどうか(1 or 0)
+    // for(int i=0;i<F.size();i++){
+    //     if(overh(angle,direction,V[F[i](0)],V[F[i](1)],V[F[i](2)])) num.push_back(1);
+    //     else num.push_back(0);
+    // }
+
+    // // vtk出力，結果の可視化
+    // FILE* fpv;
+    // fpv=fopen("overhang.vtk","w");
+    // fprintf(fpv,"# vtk DataFile Version 2.0\n");
+    // fprintf(fpv,"Title of my super cool VTK file\nASCII\nDATASET UNSTRUCTURED_GRID\n");
+    // fprintf(fpv,"POINTS %d float\n",(int)V.size());
+    // for(int i=0;i<V.size();i++){
+    //     fprintf(fpv,"%lf %lf %lf\n",V[i](0),V[i](1),V[i](2));
+    // }
+    // fprintf(fpv,"CELLS %d %d\n",(int)F.size(),(int)F.size()*4);
+    // for(int i=0;i<F.size();i++){
+    //     fprintf(fpv,"3 %d %d %d\n",F[i](0),F[i](1),F[i](2));
+    // }
+    // fprintf(fpv,"CELL_TYPES %d\n",(int)F.size());
+    // for(int i=0;i<F.size();i++){
+    //     fprintf(fpv,"5\n");
+    // }
+    // fprintf(fpv,"CELL_DATA %d\n",(int)F.size());
+    // fprintf(fpv,"SCALARS my_face_attr int 1\nLOOKUP_TABLE default\n");
+    // for(int i=0;i<num.size();i++){
+    //     fprintf(fpv,"%d\n",num[i]);
+    // }
     // cout<<"numsize="<<num.size()<<endl;
 }
 
